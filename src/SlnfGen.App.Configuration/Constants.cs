8de399b00ad28f7d12c1b16a4d43420a1dd28f82
EndreLur <endre.had@gmail.com>
2026-05-16 19:24:23 +0200

    add

---
+++ src/SlnfGen.App.Configuration/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SlnfGen.App.Configuration;
+
+public class Constants
+{
+    /// <summary>
+    ///     Expected suffix of configuration files.
+    /// </summary>
+    public const string ConfigFileSuffix = ".slnfconfig";
+}
