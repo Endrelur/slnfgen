4325f7028932076a1f37076e5a91fb775c5a1f62
EndreLur <endre.had@gmail.com>
2026-05-16 20:06:02 +0200

    Add Result.cs

---
+++ src/SlnfGen.Utils/Results/Result.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SlnfGen.Utils.Results;
+
+public struct SuccessResult();
+
+public class Result<TError>
+{
+    protected Result(){ }
+
+    [MemberNotNullWhen(false, nameof(Error))]
+    public required virtual bool Success { get; init; }
+    
+    [MemberNotNullWhen(true, nameof(Error))]
+    public bool Failure => !Success;
+
+    public TError? Error { get; init; }
+    
+    public static implicit operator Result<TError>(TError error) =>
+        new()
+        {
+            Success = false,
+            Error = error,
+        };
+    
+    public static implicit operator Result<TError>(bool success) =>
+        new()
+        {
+            Success = success,
+        };
+    
+    public static implicit operator Result<TError>(SuccessResult _) =>
+        new()
+        {
+            Success = true,
+        };
+}
+
+public class Result<TValue, TError> : Result<TError>
+{
+    private Result(){ }
+
+    [MemberNotNullWhen(true, nameof(Value))]
+    public required override bool Success { get; init; }
+
+    public TValue? Value { get; init; }
+
+    public static implicit operator Result<TValue, TError>(TError error) =>
+        new()
+        {
+            Error = error,
+            Success = false,
+        };
+    
+    public static implicit operator Result<TValue, TError>(TValue value) =>
+        new()
+        {
+            Value = value,
+            Success = true,
+        };
+}
